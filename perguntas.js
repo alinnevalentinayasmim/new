636ea153612846405bf99403a53b882e66aaf9bc
alinnevalentinayasmim <161481905+alinnevalentinayasmim@users.noreply.github.com>
2024-10-10 11:52:00 -0300

    t

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criaCartao(
+    'matemática',
+    'como se calcula a soma dos termos de uma P.A?',
+    'utiliza-se a fórmula a seguir: Sn=(a1+ano)x n!')
+    
+    criaCartao(
+    'historia',
+    'quem foram os principais líderes das potências dos eixos, na segunda queria mundial?',
+    'Adolf Hitler (Alemanha), Benito Mussolini (Itália) e Imperador Hirohito (Japão)')
+    
+    criaCartao(
+    'historia',
+    'Qual foi o evento que deu início à Segunda Guerra Mundial?',
+    'A invasão da Polônia pela Alemanha em 1º de setembro de 1939.')
+    
+    criaCartao(
+    'historia',
+    'O que foi o Holocausto?',
+    'O genocídio sistemático, perpetrado pelos nazistas, que resultou na morte de aproximadamente seis milhões de judeus e milhões de outras pessoas.')
+    
+    criaCartao(
+    'historia',
+    'Em que ano foi lançado o primeiro ataque nuclear durante a Segunda Guerra Mundial?',
+    'Em 6 de agosto de 1945, sobre a cidade de Hiroshima, Japão.')
+    
+    criaCartão(
+    'historia',
+    'O que foi a Revolução Francesa?',
+    'Um período de grande agitação social e política na França, que começou em 1789 e levou à queda da monarquia e ao surgimento da República.')
+    
+    criaCartão(
+    'historia',
+    'Quem foi Napoleão Bonaparte?',
+    'Um general francês que se tornou imperador da França e dominou grande parte da Europa no início do século XIX.')
+    
+    criaCartão(
+    'historia',
+    'O que foi o Tratado de Versalhes?',
+    'Um tratado de paz assinado em 1919 que encerrou a Primeira Guerra Mundial e impôs condições severas à Alemanha.')
+    
+    criaCartão(
+    'historia',
+    'Quando ocorreu a Guerra Civil Americana?',
+    'Entre 1861 e 1865.')
+
+    criaCartão(
+    'historia',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando da Áustria e a complexa rede de alianças e rivalidades europeias.')
+    
+    criaCartão(
+    'historia',
+    'Quem foi o líder da Revolução Russa de 1917?',
+    'Vladimir Lenin.')
+    
+    criaCartão(
+    'historia',
+    'O que foi a Guerra Fria?',
+    'Um período de tensão política e militar entre os Estados Unidos e a União Soviética, que durou aproximadamente de 1947 a 1991.')
+    
+    
+    
+    criaCartão(
+    'historia',
+    'Quando ocorreu a Independência do Brasil?',
+    'Em 7 de setembro de 1822.')
+    
+    criaCartão(
+    'historia',
+    'Qual foi o impacto da Revolução Industrial?',
+    'A Revolução Industrial, que começou no final do século XVIII, trouxe mudanças tecnológicas e sociais profundas, incluindo a urbanização e a mecanização da produção.')
+    
+    criaCartão(
+    'historia',
+    ' O que foi o Renascimento?',
+    'Um movimento cultural e artístico que floresceu na Europa entre os séculos XIV e XVII, caracterizado pela redescoberta da arte e ciência clássicas.')
+    
+    criaCartão(
+    'historia',
+    'Quem foi o autor da obra "O Príncipe"?',
+    'Nicolau Maquiavel.')
+    
+    criaCartão(
+    'historia',
+    'O que foi a Inconfidência Mineira?',
+    'Um movimento separatista no Brasil colonial, ocorrido em 1789, que buscava a independência da capitania de Minas Gerais.')
+    
+    criaCartão(
+    'historia',
+    'O que foi a Magna Carta?',
+    'Um documento assinado em 1215 que limitava os poderes do rei João da Inglaterra e estabelecia certos direitos para os nobres.')
+    
+    
+    criaCartão(
+    'historia',
+    'Qual foi o objetivo do Movimento dos Direitos Civis nos Estados Unidos?',
+    'Buscar a igualdade de direitos e o fim da segregação racial, especialmente durante as décadas de 1950 e 1960.')
+    
+    criaCartão(
+    'historia',
+    'O que foi a Restauração Meiji no Japão?',
+    'Um período de modernização e reforma no Japão que começou em 1868 e terminou com a transformação do país em uma potência industrial.')
