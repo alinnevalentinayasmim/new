05dd0ff2bea0bd7badfdc3ec91e8496067377c58
alinnevalentinayasmim <161481905+alinnevalentinayasmim@users.noreply.github.com>
2024-10-17 11:47:28 -0300

    GT

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function criaCartao(categoria,pergunta, resposta) {
     cartao.localName = 'cartao';
     cartao.innerHTML = `
    <div class="conteudo-cartao">
-<h3>${categorias}</h3>
+<h3>${categoria}</h3>
 <div class="pergunta-cartao">
 <p>${pergunta}</p>
 </div>
