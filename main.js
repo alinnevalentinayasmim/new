83c88609bc3eb1bb2d4e35b84ec79806b0349649
alinnevalentinayasmim <161481905+alinnevalentinayasmim@users.noreply.github.com>
2024-10-30 07:41:26 -0300

    m

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-function criaCartao(categoria,pergunta, resposta) {
+function criarCartao(Categoria,Pergunta,Resposta){
     let container = document.getElementById('container');
-    let cartao = document.createElement('article');
+    let cartao = document.createElement('articicle');
     cartao.className = 'cartao';
-    cartao.innerHTML = `
-   <div class="conteudo-cartao">
-<h3>${categoria}</h3>
-<div class="pergunta-cartao">
-<p>${pergunta}</p>
-</div>
-<div class="resposta-cartao">
-    <p>${resposta}</p>
-</div>
-</div>
-`
-container.appendChild(cartao);
-}
+    cartao.innerHTML =  `
+      <div class="conteudo-cartao">
+      <h3>${Categoria}</h3>
+    <div class="pergunta-cartao">
+      <p>${Pergunta}</p>
+    </div>
+    <div class="resposta-cartao">
+      <p>${Resposta}</p>
+      </div>
+  </div>
+     `
+     let RespostaEstaVisivel = false;
+     function viracartao(){
+      RespostaEstaVisivel = !RespostaEstaVisivel;
+      cartao.classList.toggle('active', RespostaEstaVisivel);
+     }
+     cartao.addEventListener('click', viracartao);
+     
+     container.appendChild(cartao);
+  }
+  
