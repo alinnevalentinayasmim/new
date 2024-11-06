7045fc94dd1fc513816f994780b5207972c5d169
alinnevalentinayasmim <161481905+alinnevalentinayasmim@users.noreply.github.com>
2024-11-06 07:55:30 -0300

    m,

--- pagina2.js
+++ pagina2.js
@@ -13,6 +13,16 @@ function criaCartao(Categoria,Pergunta,Resposta){
       </div>
   </div>
      `
+    
      container.appendChild(cartao);
   }
   
+  function mostraResposta (card){
+    const resposta=card.querySelector('.resposta-cartao');
+    if(resposta=style.display==='none'){
+      resposta.style.display='block';
+  }
+  else{
+    resposta.style.display='none';
+  }
+}
